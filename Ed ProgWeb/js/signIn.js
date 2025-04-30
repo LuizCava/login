12740e28f42e7358522676ff63f89547dce2fe3e
LuizCava <luizcava02@gmail.com>
2025-04-29 21:24:06 -0300

    Add files via upload

--- Ed ProgWeb/js/signIn.js
+++ Ed ProgWeb/js/signIn.js
@@ -7,7 +7,6 @@ document.getElementById('signForm').addEventListener('submit', function(event) {
     const user = userData[0];
 
     if (username === user.name && password === user.password) {
-        alert('Login successful!');
         window.location.href = 'home.html';
     } else {
         alert('Invalid credentials, please try again.');
